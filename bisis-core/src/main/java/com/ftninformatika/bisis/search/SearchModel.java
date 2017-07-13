7913513b7f3753f336d9c1ca6b80ccf2d814d6c6
Petar Makevic <jiricekova31@gmail.com>
2017-07-13 16:04:49 +0200

    Search model makeQuery

--- bisis-core/src/main/java/com/ftninformatika/bisis/search/SearchModel.java
+++ bisis-core/src/main/java/com/ftninformatika/bisis/search/SearchModel.java
@@ -4,8 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
-
+import org.elasticsearch.index.query.QueryBuilders;
 /**
  * Created by Petar on 7/13/2017.
  */
@@ -33,9 +34,56 @@ public class SearchModel {
 
 
     public QueryBuilder makeQuery(){
-        QueryBuilder retVal = null;
+        BoolQueryBuilder retVal = QueryBuilders.boolQuery();
 
-        
+
+        try {
+            if (!"".equals(text1))
+                retVal.must(QueryBuilders.matchPhrasePrefixQuery(pref1, text1));
+
+            if (!"".equals(text2)) {
+                if ( "AND".equals(oper1))
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery(pref2, text2));
+                if ( "OR".equals(oper1)) {
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery(pref2, text2));
+                }
+                if ( "NOT".equals(oper1))
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery(pref2, text2));
+            }
+
+            if (!"".equals(text3)) {
+                if ( "AND".equals(oper2))
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery(pref3, text3));
+                if ( "OR".equals(oper2))
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery(pref3, text3));
+                if ( "NOT".equals(oper2))
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery(pref3, text3));
+            }
+
+            if (!text4.equals("")) {
+            } else {
+                if ( "AND".equals(oper3))
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery(pref4, text4));
+                if ( "OR".equals(oper3))
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery(pref4, text4));
+                if ( "NOT".equals(oper3))
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery(pref4, text4));
+            }
+
+            if (!"".equals(text5)) {
+                if ( "AND".equals(oper4))
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery(pref5, text5));
+                if ( "OR".equals(oper4))
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery(pref5, text5));
+                if ( "NOT".equals(oper4))
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery(pref5, text5));
+            }
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
+
 
         return retVal;
     }
