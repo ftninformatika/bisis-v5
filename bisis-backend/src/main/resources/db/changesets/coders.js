4a02f54bca866ead0d35c2d3360014fe1cf96ef2
dboberic <dboberic@uns.ac.rs>
2017-07-26 12:58:50 +0200

    init za sifarnik lokacija

--- bisis-backend/src/main/resources/db/changesets/coders.js
+++ bisis-backend/src/main/resources/db/changesets/coders.js
@@ -4,18 +4,25 @@
 //changeset petar:ChangeSet-coders
 db.coders.status.insert(
   [
-      {'status': '+', 'opis': 'Slobodno za razmenu', 'zaduziv':0},
-      {'status': '-', 'opis': 'Deziderat', 'zaduziv':0},
-      {'status': '1', 'opis': 'Naručeno', 'zaduziv':0},
-      {'status': '2', 'opis': 'U obradi', 'zaduziv':0},
-      {'status': '3', 'opis': 'U povezu', 'zaduziv':0},
-      {'status': '4', 'opis': 'U reviziji', 'zaduziv':0},
-      {'status': '5', 'opis': 'Preusmereno', 'zaduziv':1},
-      {'status': '6', 'opis': 'Oštećeno', 'zaduziv':0},
-      {'status': '7', 'opis': 'Zagubljeno', 'zaduziv':0},
-      {'status': '8', 'opis': 'Izgubljeno', 'zaduziv':0},
-      {'status': '9', 'opis': 'Otpisano', 'zaduziv':2},
-      {'status': 'A', 'opis': 'Aktivno', 'zaduziv':1},
-      {'status': 'E', 'opis': 'Britanski Savet', 'zaduziv':0}
+      {'status': '+', 'description': 'Slobodno za razmenu', 'lendable':false, 'showable':false},
+      {'status': '-', 'description': 'Deziderat'},
+      {'status': '1', 'description': 'Naručeno'},
+      {'status': '2', 'description': 'U obradi'},
+      {'status': '3', 'description': 'U povezu'},
+      {'status': '4', 'description': 'U reviziji'},
+      {'status': '5', 'description': 'Preusmereno'},
+      {'status': '6', 'description': 'Oštećeno'},
+      {'status': '7', 'description': 'Zagubljeno'},
+      {'status': '8', 'description': 'Izgubljeno'},
+      {'status': '9', 'description': 'Otpisano'},
+      {'status': 'A', 'description': 'Aktivno'},
+      {'status': 'E', 'description': 'Britanski Savet'}
   ]
+);
+
+db.coders.location.insert(
+    [
+        {'library':'gbns','location_id': '01', 'location_name': 'Stevan Sremac'}
+
+    ]
 );
