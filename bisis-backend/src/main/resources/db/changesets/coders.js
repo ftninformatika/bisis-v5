caf6014f58eb78a4083b78dd6e4574a998abebb2
dboberic <dboberic@uns.ac.rs>
2017-07-28 15:51:10 +0200

    init sifarnika

--- bisis-backend/src/main/resources/db/changesets/coders.js
+++ bisis-backend/src/main/resources/db/changesets/coders.js
@@ -270,4 +270,35 @@ db.coders.sublocation.insert(
 
     ]
 );
+db.coders.education.insert(
+    [
+        { 'description': 'osnovna škola'},
+        { 'description': 'srednja škola'}
+
+    ]
+);
+db.coders.membership.insert(
+    [
+        {'memberType':'redovan','userCateg':'zaposlen','cost':500.0 }
+
+    ]
+);
+db.coders.coders.membership_type.insert(
+    [
+        { 'description': 'redovan', 'period':365}
+
+    ]
+);
+db.coders.coders.user_categ.insert(
+    [
+        {'description':'zaposlen','titlesNo':500, 'period':30, 'maxPeriod':500 }
+
+    ]
+);
+db.coders.coders.place.insert(
+    [
+        {'city':'Novi Sad', 'zip':'21000' }
+
+    ]
+);
 
