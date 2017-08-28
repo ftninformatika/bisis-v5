a38f95a69d28e750ff5a28b3a9a399ecf3eabae4
Petar Makevic <jiricekova31@gmail.com>
2017-08-28 10:43:31 +0200

    U mongeez members ubaceno nekoliko korisnika iz eksporta

--- bisis-backend/src/main/resources/db/changesets/members.js
+++ bisis-backend/src/main/resources/db/changesets/members.js
@@ -2,126 +2,492 @@
 
 
 //changeset petar:ChangeSet-memebers
-db.gbns_com_members.insert({
-    "_id" : ObjectId("59391fc3ea9b8fbe1ed5ed28"),
-    "sysId" : 12,
-    "organizationId" : 0,
-    "languages" : 0,
-    "educationLevel" : 0,
-    "membershipType" : {
-        "description": "Opis vrste clanarine",
-        "period": 365
+db.gbns_com_members.insert([
+    {
+        "_id" : ObjectId("59a3d674650e0b923884e5a5"),
+        "organization" : null,
+        "languages" : "Nepoznat jezik",
+        "educationLevel" : "Nepoznato",
+        "membershipType" : {
+            "library" : "gbns",
+            "description" : "R (redovno plaćanje)",
+            "period" : 365
+        },
+        "userCategory" : {
+            "library" : "gbns",
+            "description" : "Učenici srednjih škola",
+            "titlesNo" : 15,
+            "period" : 21,
+            "maxPeriod" : 0
+        },
+        "corporateMember" : {
+            "library" : null,
+            "userId" : "0",
+            "instName" : "x",
+            "signDate" : ISODate("1999-12-31T23:00:00.000Z"),
+            "address" : "x",
+            "city" : "x",
+            "zip" : 123,
+            "phone" : null,
+            "email" : null,
+            "fax" : null,
+            "secAddress" : null,
+            "secZip" : 0,
+            "secCity" : null,
+            "secPhone" : null,
+            "contFirstName" : null,
+            "contLastName" : null,
+            "contEmail" : null
+        },
+        "groups" : 0,
+        "userId" : "01000000000",
+        "firstName" : "Milka",
+        "lastName" : "Grubešić",
+        "parentName" : "Ilija",
+        "address" : "Veselina Masleše 146",
+        "city" : "Novi Sad",
+        "zip" : 21000,
+        "phone" : null,
+        "email" : null,
+        "jmbg" : null,
+        "docId" : 0,
+        "docNo" : "8028424325/97",
+        "docCity" : "NS",
+        "country" : null,
+        "gender" : "F",
+        "age" : "C",
+        "secAddress" : null,
+        "secZip" : 0,
+        "secCity" : null,
+        "secPhone" : null,
+        "note" : "Futog Poljoprivredna škola",
+        "interests" : null,
+        "warningInd" : 1,
+        "occupation" : null,
+        "title" : null,
+        "indexNo" : null,
+        "classNo" : 0,
+        "pass" : null,
+        "blockReason" : null,
+        "lendings" : [],
+        "signings" : [],
+        "duplicates" : [],
+        "picturebooks" : []
     },
-    "userCategory" : {
-        "description": "Opis kategorije korisnika",
-        "titlesNo": 2,
-        "period": 7,
-        "maxperiod": 14
+
+    /* 2 */
+    {
+        "_id" : ObjectId("59a3d674650e0b923884e5a6"),
+        "organization" : null,
+        "languages" : "Nepoznat jezik",
+        "educationLevel" : "Visoko",
+        "membershipType" : {
+            "library" : "gbns",
+            "description" : "B1 (službena zaduženja)",
+            "period" : 365
+        },
+        "userCategory" : {
+            "library" : "gbns",
+            "description" : "Zaposleni",
+            "titlesNo" : 15,
+            "period" : 21,
+            "maxPeriod" : 0
+        },
+        "corporateMember" : {
+            "library" : null,
+            "userId" : "0",
+            "instName" : "x",
+            "signDate" : ISODate("1999-12-31T23:00:00.000Z"),
+            "address" : "x",
+            "city" : "x",
+            "zip" : 123,
+            "phone" : null,
+            "email" : null,
+            "fax" : null,
+            "secAddress" : null,
+            "secZip" : 0,
+            "secCity" : null,
+            "secPhone" : null,
+            "contFirstName" : null,
+            "contLastName" : null,
+            "contEmail" : null
+        },
+        "groups" : 0,
+        "userId" : "01000000001",
+        "firstName" : "NOVOG SADA",
+        "lastName" : "ENCIKLOPEDIJA",
+        "parentName" : "pOPOV-kABIĆ",
+        "address" : "AKADEMIJA NAUKA",
+        "city" : "Novi Sad",
+        "zip" : 21000,
+        "phone" : "6621-719",
+        "email" : "",
+        "jmbg" : "1993200602008",
+        "docId" : 0,
+        "docNo" : "",
+        "docCity" : "Novi Sad",
+        "country" : "",
+        "gender" : "F",
+        "age" : "A",
+        "secAddress" : "",
+        "secZip" : 0,
+        "secCity" : "",
+        "secPhone" : "",
+        "note" : "",
+        "interests" : "",
+        "warningInd" : 0,
+        "occupation" : "",
+        "title" : "",
+        "indexNo" : "",
+        "classNo" : 0,
+        "pass" : null,
+        "blockReason" : "",
+        "lendings" : [],
+        "signings" : [
+            {
+                "id" : 64451,
+                "signDate" : ISODate("2009-11-03T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2010-11-03T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 79413,
+                "signDate" : ISODate("2010-11-26T23:00:00.000Z"),
+                "location" : 23,
+                "untilDate" : ISODate("2011-11-26T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 169753,
+                "signDate" : ISODate("2015-02-10T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2016-02-10T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 192641,
+                "signDate" : ISODate("2016-03-10T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2017-03-10T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 219230,
+                "signDate" : ISODate("2017-04-25T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2018-04-25T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "rashod"
+            }
+        ],
+        "duplicates" : [],
+        "picturebooks" : [
+            {
+                "lendDate" : ISODate("2008-07-07T23:00:00.000Z"),
+                "lendNo" : 1,
+                "returnNo" : 0,
+                "status" : 1
+            }
+        ]
     },
-    "groups" : 0,
-    "userId" : "01000000000",
-    "firstName" : "Milka",
-    "lastName" : "Grubešić",
-    "parentName" : "Ilija",
-    "address" : "Veselina Masleše 146",
-    "city" : "Novi Sad",
-    "zip" : 21000,
-    "phone" : null,
-    "email" : null,
-    "jmbg" : null,
-    "docId" : 0,
-    "docNo" : "8028424325/97",
-    "docCity" : "NS",
-    "country" : null,
-    "gender" : "F",
-    "age" : "C",
-    "secAddress" : null,
-    "secZip" : 0,
-    "secCity" : null,
-    "secPhone" : null,
-    "note" : "Futog Poljoprivredna škola",
-    "interests" : null,
-    "warningInd" : 1,
-    "occupation" : null,
-    "title" : null,
-    "indexNo" : null,
-    "classNo" : 0,
-    "pass" : null,
-    "blockReason" : null,
-    "lendings" : [
-        {
-            "id" : 339,
-            "ctlgNo" : "17000019566",
-            "lendDate" : ISODate("2002-12-15T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : null,
-            "resumeDate" : null,
-            "deadline" : ISODate("2003-01-04T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : null,
-            "librarianResume" : null
-        }
-        ,
-        {
-            "id" : 357,
-            "ctlgNo" : "01000062181",
-            "lendDate" : ISODate("2004-04-14T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : ISODate("2004-05-12T23:00:00.000Z"),
-            "resumeDate" : null,
-            "deadline" : ISODate("2004-05-04T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : "demo",
-            "librarianResume" : null
+
+    /* 3 */
+    {
+        "_id" : ObjectId("59a3d674650e0b923884e5a7"),
+        "organization" : null,
+        "languages" : "Nepoznat jezik",
+        "educationLevel" : "Nepoznato",
+        "membershipType" : {
+            "library" : "gbns",
+            "description" : "B1 (službena zaduženja)",
+            "period" : 365
         },
-        {
-            "id" : 358,
-            "ctlgNo" : "01000044428",
-            "lendDate" : ISODate("2004-05-12T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : ISODate("2004-05-13T23:00:00.000Z"),
-            "resumeDate" : null,
-            "deadline" : ISODate("2004-06-01T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : "demo",
-            "librarianResume" : null
+        "userCategory" : {
+            "library" : "gbns",
+            "description" : "Zaposleni",
+            "titlesNo" : 15,
+            "period" : 21,
+            "maxPeriod" : 0
         },
-        {
-            "id" : 359,
-            "ctlgNo" : "01000087566",
-            "lendDate" : ISODate("2004-05-12T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : ISODate("2004-05-23T23:00:00.000Z"),
-            "resumeDate" : null,
-            "deadline" : ISODate("2004-06-01T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : "demo",
-            "librarianResume" : null
+        "corporateMember" : {
+            "library" : null,
+            "userId" : "0",
+            "instName" : "x",
+            "signDate" : ISODate("1999-12-31T23:00:00.000Z"),
+            "address" : "x",
+            "city" : "x",
+            "zip" : 123,
+            "phone" : null,
+            "email" : null,
+            "fax" : null,
+            "secAddress" : null,
+            "secZip" : 0,
+            "secCity" : null,
+            "secPhone" : null,
+            "contFirstName" : null,
+            "contLastName" : null,
+            "contEmail" : null
         },
-        {
-            "id" : 360,
-            "ctlgNo" : "01000107073",
-            "lendDate" : ISODate("2004-05-12T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : ISODate("2004-05-13T23:00:00.000Z"),
-            "resumeDate" : null,
-            "deadline" : ISODate("2004-06-01T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : "demo",
-            "librarianResume" : null
+        "groups" : 0,
+        "userId" : "01000000003",
+        "firstName" : "Marija",
+        "lastName" : "Peković",
+        "parentName" : "Milan",
+        "address" : "Cara Dušana 45",
+        "city" : "Novi Sad",
+        "zip" : 21000,
+        "phone" : "300-48-00  064/614-35-24",
+        "email" : "",
+        "jmbg" : "0612970815037",
+        "docId" : 0,
+        "docNo" : "004572929/13",
+        "docCity" : "novi Sad",
+        "country" : "",
+        "gender" : "F",
+        "age" : "A",
+        "secAddress" : "",
+        "secZip" : 0,
+        "secCity" : "",
+        "secPhone" : "",
+        "note" : "Gradska biblioteka",
+        "interests" : "",
+        "warningInd" : 1,
+        "occupation" : "",
+        "title" : "",
+        "indexNo" : "",
+        "classNo" : 0,
+        "pass" : null,
+        "blockReason" : "",
+        "lendings" : [],
+        "signings" : [
+            {
+                "id" : 16,
+                "signDate" : ISODate("2005-10-20T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2006-10-20T23:00:00.000Z"),
+                "cost" : null,
+                "receiptId" : null,
+                "librarian" : "smena2"
+            },
+            {
+                "id" : 17,
+                "signDate" : ISODate("2006-11-29T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2007-11-29T23:00:00.000Z"),
+                "cost" : null,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 18,
+                "signDate" : ISODate("2008-02-13T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2009-02-12T23:00:00.000Z"),
+                "cost" : null,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 57387,
+                "signDate" : ISODate("2009-04-09T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2010-04-09T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : "0",
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 72387,
+                "signDate" : ISODate("2010-06-04T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2011-06-04T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 86193,
+                "signDate" : ISODate("2011-05-29T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2012-05-28T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 114857,
+                "signDate" : ISODate("2012-05-29T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2013-05-29T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena2"
+            },
+            {
+                "id" : 133396,
+                "signDate" : ISODate("2013-05-15T23:00:00.000Z"),
+                "location" : 21,
+                "untilDate" : ISODate("2014-05-15T23:00:00.000Z"),
+                "cost" : 800,
+                "receiptId" : "003433/1",
+                "librarian" : "ljiljakos"
+            },
+            {
+                "id" : 166990,
+                "signDate" : ISODate("2014-12-22T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2015-12-22T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 187216,
+                "signDate" : ISODate("2015-12-09T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2016-12-08T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            }
+        ],
+        "duplicates" : [
+            {
+                "dupDate" : ISODate("2010-09-17T23:00:00.000Z"),
+                "dupNo" : 1
+            }
+        ],
+        "picturebooks" : [
+            {
+                "lendDate" : ISODate("2008-07-07T23:00:00.000Z"),
+                "lendNo" : 1,
+                "returnNo" : 0,
+                "status" : 1
+            }
+        ]
+    },
+
+    /* 4 */
+    {
+        "_id" : ObjectId("59a3d674650e0b923884e5a8"),
+        "organization" : null,
+        "languages" : "Srpski - ćirilica",
+        "educationLevel" : "Srednje",
+        "membershipType" : {
+            "library" : "gbns",
+            "description" : "B1 (službena zaduženja)",
+            "period" : 365
         },
-        {
-            "id" : 361,
-            "ctlgNo" : "01000038834",
-            "lendDate" : ISODate("2004-05-13T23:00:00.000Z"),
-            "location" : 1,
-            "returnDate" : ISODate("2004-05-23T23:00:00.000Z"),
-            "resumeDate" : null,
-            "deadline" : ISODate("2004-06-02T23:00:00.000Z"),
-            "librarianLend" : "demo",
-            "librarianReturn" : "demo",
-            "librarianResume" : null
-        }
-    ],
-    "signings" : []
-});
+        "userCategory" : {
+            "library" : "gbns",
+            "description" : "Zaposleni",
+            "titlesNo" : 15,
+            "period" : 21,
+            "maxPeriod" : 0
+        },
+        "corporateMember" : {
+            "library" : null,
+            "userId" : "0",
+            "instName" : "x",
+            "signDate" : ISODate("1999-12-31T23:00:00.000Z"),
+            "address" : "x",
+            "city" : "x",
+            "zip" : 123,
+            "phone" : null,
+            "email" : null,
+            "fax" : null,
+            "secAddress" : null,
+            "secZip" : 0,
+            "secCity" : null,
+            "secPhone" : null,
+            "contFirstName" : null,
+            "contLastName" : null,
+            "contEmail" : null
+        },
+        "groups" : 0,
+        "userId" : "01000000004",
+        "firstName" : "Jovana",
+        "lastName" : "Kabić",
+        "parentName" : "Danilo",
+        "address" : "Gimnazijska 1",
+        "city" : "Novi Sad",
+        "zip" : 21000,
+        "phone" : "6619-713 063/8-215-200",
+        "email" : "",
+        "jmbg" : "2801984805047",
+        "docId" : 0,
+        "docNo" : "",
+        "docCity" : "",
+        "country" : "",
+        "gender" : "F",
+        "age" : "A",
+        "secAddress" : "",
+        "secZip" : 0,
+        "secCity" : "",
+        "secPhone" : "",
+        "note" : "",
+        "interests" : "",
+        "warningInd" : 0,
+        "occupation" : "",
+        "title" : "",
+        "indexNo" : "",
+        "classNo" : 0,
+        "pass" : null,
+        "blockReason" : "",
+        "lendings" : [],
+        "signings" : [
+            {
+                "id" : 173386,
+                "signDate" : ISODate("2015-04-15T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2016-04-14T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena2"
+            },
+            {
+                "id" : 194387,
+                "signDate" : ISODate("2016-04-17T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2017-04-17T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            },
+            {
+                "id" : 218213,
+                "signDate" : ISODate("2017-04-05T23:00:00.000Z"),
+                "location" : 1,
+                "untilDate" : ISODate("2018-04-05T23:00:00.000Z"),
+                "cost" : 0,
+                "receiptId" : null,
+                "librarian" : "smena1"
+            }
+        ],
+        "duplicates" : [
+            {
+                "dupDate" : ISODate("2012-01-05T23:00:00.000Z"),
+                "dupNo" : 1
+            }
+        ],
+        "picturebooks" : [
+            {
+                "lendDate" : ISODate("2008-07-08T23:00:00.000Z"),
+                "lendNo" : 1,
+                "returnNo" : 0,
+                "status" : 1
+            }
+        ]
+    }
+
+]);
