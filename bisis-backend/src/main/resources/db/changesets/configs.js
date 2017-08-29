91c1193ba12d1de76f007a32c2ab6bf0875188c7
Petar Makevic <jiricekova31@gmail.com>
2017-08-29 14:45:11 +0200

    Prosiren model LibraryConfiguration i mongeeze poljima iz client-cong.ini

--- bisis-backend/src/main/resources/db/changesets/configs.js
+++ bisis-backend/src/main/resources/db/changesets/configs.js
@@ -4,18 +4,48 @@
 //changeset petar:ChangeSet-configs
 db.configs.insert({
     "libraryName" : "gbns",
-    "primerciModel" : "0 1 2 3 13 10 11",
+
+    "networkServerList": "http://bisis.uns.ac.rs/servers/bisisXMLM/servers.xml",
+
+    "catalougingPrimerciModel" : "0 1 2 3 13 10 11",
     "catalougingGodineModel" : "0 1 2 3 12 13 15",
     "catalougingInvbrSubStr" : "1 4",
     "catalougingDefaultPrimerakInvKnj" : "00",
-    "catalougingDefaultSveskaInvKnj" : "99",
+    "catalougingDefaultSveskaInvKnj" : "00",
     "catalougingDefaultGodinaInvKnj" : "00",
     "catalougingValidator" : "com.gint.app.bisis4.client.editor.validation.GBValidator",
     "catalougingReportset" : "gbbg",
+
+    "bookcardsLocale": "gbns",
     "bookcardsNextPage" : "Još...",
-    "bookcardsCurrentType" : "monografski",
+    "bookcardsCurrentType" : "glavni",
     "bookcardsTranslateX" : "15",
     "bookcardsTranslateY" : "5",
     "bookcardsFontSize" : "-1",
-    "bookcardsBrRedova" : "13"
+    "bookcardsBrRedova" : "13",
+
+    "barcodePort": "lpt",
+    "barcodeOptionName": "library1",
+    "barcodeLibrary1": "БИБЛИОТЕКА ШАБАЧКА",
+    "barcodeLabelWidth": "456",
+    "barcodeLabelHeight": "0",
+    "barcodeLabelResolution": "203",
+    "barcodeBarwidth": "140",
+    "barcodeNarrowbar": "2",
+    "barcodeWidebar": "3",
+    "barcodeSigfont": "4",
+    "barcodeLabelfont": "3",
+    "barcodePageCode": "1251",
+    "barcodeWrap": "26",
+
+    "barcodeUsersLabelWidth": "300",
+    "barcodeUsersLabelHeight": "0",
+    "barcodeUsersLabelResolution": "203",
+    "barcodeUsersBarwidth": "50",
+    "barcodeUsersNarrowbar": "2",
+    "barcodeUsersWidebar": "2",
+    "barcodeUsersPageCode": "1251",
+    "barcodeUsersLinelength": "19"
+
+
 });
