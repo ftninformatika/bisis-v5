494becd6f33dd757cf6a61cb02635a4b5d875ba5
Petar Makevic <jiricekova31@gmail.com>
2017-10-12 11:39:20 +0200

    Wrapper model za response pretrage

---
+++ bisis-core/src/main/java/com/ftninformatika/bisis/records/RecordResponseWrapper.java
@@ -0,0 +1,22 @@
+package com.ftninformatika.bisis.records;
+
+import com.ftninformatika.bisis.prefixes.ElasticPrefixEntity;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * Created by Petar on 10/12/2017.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RecordResponseWrapper {
+
+    private ElasticPrefixEntity prefixEntity;
+    private Record fullRecord;
+    private List<StanjePrimerka> listOfItems;
+
+}
---
+++ bisis-core/src/main/java/com/ftninformatika/bisis/records/StanjePrimerka.java
@@ -0,0 +1,17 @@
+package com.ftninformatika.bisis.records;
+
+import lombok.*;
+
+/**
+ * Created by Petar on 10/12/2017.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class StanjePrimerka {
+
+    private String primerakId;
+    private boolean slobodan;
+}
