a4b7df23f92bfd980663be29606243a5eb2c24ce
dboberic <dboberic@uns.ac.rs>
2017-10-25 12:32:21 +0200

    dodat profile

--- bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/Application.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/Application.java
@@ -1,26 +1,21 @@
 package com.ftninformatika.bisis.rest_service;
 
-import com.ftninformatika.bisis.librarian.Librarian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
-
-@SpringBootApplication
+@Profile("development")
 @EnableMongoRepositories("com.ftninformatika")
 @EnableElasticsearchRepositories("com.ftninformatika")
-@ComponentScan("com.ftninformatika")
 @EntityScan("com.ftninformatika")
+@SpringBootApplication
+
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
 }
