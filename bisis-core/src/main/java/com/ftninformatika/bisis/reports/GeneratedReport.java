6881ecf0ce8a2a8b43586e3a08d7810358b07416
dboberic <dboberic@uns.ac.rs>
2017-10-25 18:45:03 +0200

    izvestaj

---
+++ bisis-core/src/main/java/com/ftninformatika/bisis/reports/GeneratedReport.java
@@ -0,0 +1,23 @@
+package com.ftninformatika.bisis.reports;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+/**
+ * Created by dboberic on 15/10/2017.
+ */
+@Getter
+@Setter
+@JsonIgnoreProperties( ignoreUnknown = true )
+@Document(collection = "#{libraryPrefixProvider.getLibPrefix()}_reports")
+public class GeneratedReport implements Serializable{
+    String reportName;
+    String content;
+    String reportType;
+
+}
