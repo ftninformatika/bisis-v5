820c1b5ebdefec67ebd9d0a0e1d879c1dd561e32
dboberic <dboberic@uns.ac.rs>
2017-10-25 18:36:46 +0200

    izvestaj main klasa

--- bisis-reports/src/main/java/com/ftninformatika/bisis/ReportApplication.java
+++ bisis-reports/src/main/java/com/ftninformatika/bisis/ReportApplication.java
@@ -8,9 +8,11 @@ import com.ftninformatika.bisis.rest_service.repository.mongo.BindingRepository;
 import com.ftninformatika.bisis.rest_service.repository.mongo.LibraryConfigurationRepository;
 import com.ftninformatika.bisis.rest_service.repository.mongo.RecordsRepository;
 import com.ftninformatika.bisis.rest_service.repository.mongo.ReportsRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -18,19 +20,27 @@ import java.util.List;
 /**
  * Created by dboberic on 05/10/2017.
  */
+
 @ComponentScan("com.ftninformatika")
 public class ReportApplication  {
+
+    @Value("@{libraryPrefixProvider.getLibPrefix()}_records")
+    private static String temp;
 
     public static void main(String[] args){
 
         AnnotationConfigApplicationContext  ctx = new AnnotationConfigApplicationContext();
         ctx.getEnvironment().setActiveProfiles("reporting");
+        ctx.register(LibraryPrefixProvider.class);
         ctx.register(ReportConfig.class);
         ctx.refresh();
-       List<LibraryConfiguration> libconfigs=ctx.getBean(LibraryConfigurationRepository.class).findAll();
+
+
+        List<LibraryConfiguration> libconfigs=ctx.getBean(LibraryConfigurationRepository.class).findAll();
+        LibraryPrefixProvider libProvider = ctx.getBean(LibraryPrefixProvider.class);
+
         ReportsRepository reportRep=ctx.getBean(ReportsRepository.class);
         RecordsRepository recRep=ctx.getBean(RecordsRepository.class);
-        LibraryPrefixProvider libProvider = ctx.getBean(LibraryPrefixProvider.class);
         for (LibraryConfiguration lc:libconfigs){
             libProvider.setPrefix(lc.getLibCollectionSufix());
             ReportCollection collection = new ReportCollection(lc,reportRep,ctx.getBean(BindingRepository.class));
@@ -41,17 +51,4 @@ public class ReportApplication  {
 
     }
 
-
-
-   /* public void run(String... args) throws Exception {
-       List<LibraryConfiguration> libconfigs=ctx..findAll();
-       for (LibraryConfiguration lc:libconfigs){
-           libProvider.setPrefix(lc.getLibCollectionSufix());
-           ReportCollection collection = new ReportCollection(lc,reportRep,binRep);
-           reportRep.deleteAll(); //obrise postojece izvestaje od prethodnog dana
-           ReportRunner runner = new ReportRunner(collection,recRep);
-           runner.run();
-          // sortReports(collection.getDestination()); //sortira izvestaje
-       }
-    }*/
 }
