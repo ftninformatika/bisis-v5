eec3f7e7525a5c8f1f7961b2249d67b0b3609cfc
dboberic <dboberic@uns.ac.rs>
2017-10-25 18:56:36 +0200

    s

--- bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/repository/mongo/BindingRepository.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/repository/mongo/BindingRepository.java
@@ -10,4 +10,7 @@ public interface BindingRepository extends MongoRepository<Binding, String> {
 
     @Query("{'$or': [{'library':{'$exists': false}},{'library':?0}]}")
     public List<Binding> getCoders(String libName);
+
+    @Query("{'$or': [{'library':?0},{'coder_id':?1}]}")
+    public Binding getCoder(String library, String code);
 }
