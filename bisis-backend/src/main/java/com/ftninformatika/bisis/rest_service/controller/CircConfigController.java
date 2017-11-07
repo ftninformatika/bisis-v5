61fdb761633b6930dfb8e2d3f9798a3c075d13c4
Petar Makevic <jiricekova31@gmail.com>
2017-11-07 13:28:01 +0100

    +

--- bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/controller/CircConfigController.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/controller/CircConfigController.java
@@ -5,10 +5,8 @@ import com.ftninformatika.bisis.models.circ.CircLocation;
 import com.ftninformatika.bisis.rest_service.repository.mongo.CircConfigRepository;
 import com.ftninformatika.bisis.rest_service.repository.mongo.CircLocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +20,7 @@ public class CircConfigController {
     @Autowired
     CircConfigRepository circLocationRepository;
 
-    @RequestMapping("/save")
+    @RequestMapping( value = "/save", method = RequestMethod.POST )
     public CircConfig save(CircConfig circConfig){
         return  circLocationRepository.save(circConfig);
     }
