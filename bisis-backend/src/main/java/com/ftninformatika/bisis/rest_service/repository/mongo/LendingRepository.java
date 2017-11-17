3c0a22e7282747970b78e3e7aeb355cd5305a06e
dboberic <dboberic@uns.ac.rs>
2017-11-17 12:07:08 +0100

    servis za pretragu knjiga iz cirkulacije

--- bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/repository/mongo/LendingRepository.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/repository/mongo/LendingRepository.java
@@ -2,18 +2,21 @@ package com.ftninformatika.bisis.rest_service.repository.mongo;
 
 import com.ftninformatika.bisis.circ.Lending;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Petar on 8/28/2017.
  */
 @RepositoryRestResource(collectionResourceRel = "lendings", path = "mongo_repository_lendings")
-public interface LendingRepository extends MongoRepository<Lending, String> {
+public interface LendingRepository extends MongoRepository<Lending, String>,LendingRepositoryCustom{
 
     public List<Lending> findByUserId(@Param("userId") String userId);
 
     public List<Lending> findByUserIdAndReturnDateIsNull(@Param("userId") String userId);
+
 }
