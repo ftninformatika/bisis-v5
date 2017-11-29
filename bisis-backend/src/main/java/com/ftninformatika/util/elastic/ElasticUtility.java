980c2d627e462d6922137613b89307c97daa4118
dboberic <dboberic@uns.ac.rs>
2017-11-29 10:32:10 +0100

    pretraga po vrsti autorstva

--- bisis-backend/src/main/java/com/ftninformatika/util/elastic/ElasticUtility.java
+++ bisis-backend/src/main/java/com/ftninformatika/util/elastic/ElasticUtility.java
@@ -3,14 +3,10 @@ package com.ftninformatika.util.elastic;
 import com.ftninformatika.bisis.prefixes.ElasticPrefixEntity;
 import com.ftninformatika.bisis.search.SearchModel;
 import com.ftninformatika.bisis.search.UniversalSearchModel;
+import com.ftninformatika.utils.string.LatCyrUtils;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.springframework.data.elasticsearch.core.query.StringQuery;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -53,49 +49,56 @@ public class ElasticUtility {
 
     //formiranje Query-ja za glavnu pretragu zapisa
     public static BoolQueryBuilder makeQuery(SearchModel sm){
+
+        //ovo je zbog vrste autorstva Pera Peric mentor, sve se trazi u jednom polju AU
+        if (sm.getValueForPrefix("AU")!=null && !sm.getValueForPrefix("AU").equals("") && sm.getValueForPrefix("TA")!=null){
+            sm.setValueForPrefix("AU",sm.getValueForPrefix("AU")+" "+sm.getValueForPrefix("TA"));
+            sm.setValueForPrefix("TA","");
+        }
+
         BoolQueryBuilder retVal = QueryBuilders.boolQuery();
 //        sm.fixDepartments();
 
         try {
             if (sm.getText1() != null && !"".equals(sm.getText1()))
-                retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref1(), sm.getText1()));
+                retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref1(), LatCyrUtils.toLatinUnaccented(sm.getText1())));
 
             if (sm.getText2() != null && !"".equals(sm.getText2())) {
                 if ( "AND".equals(sm.getOper1()))
-                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref2(), sm.getText2()));
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref2(), LatCyrUtils.toLatinUnaccented(sm.getText2())));
                 if ( "OR".equals(sm.getOper1())) {
-                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref1(), sm.getText2()));
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref2(), LatCyrUtils.toLatinUnaccented(sm.getText2())));
                 }
                 if ( "NOT".equals(sm.getOper1()))
-                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref2(), sm.getText2()));
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref2(), LatCyrUtils.toLatinUnaccented(sm.getText2())));
             }
 
             if (sm.getText3() != null && !"".equals(sm.getText3())) {
                 if ( "AND".equals(sm.getOper2()))
-                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), sm.getText3()));
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), LatCyrUtils.toLatinUnaccented(sm.getText3())));
                 if ( "OR".equals(sm.getOper2()))
-                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), sm.getText3()));
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), LatCyrUtils.toLatinUnaccented(sm.getText3())));
                 if ( "NOT".equals(sm.getOper2()))
-                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), sm.getText3()));
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref3(), LatCyrUtils.toLatinUnaccented(sm.getText3())));
             }
 
             if (sm.getText4() != null && !"".equals(sm.getText4())) {
 
                 if ( "AND".equals(sm.getOper3()))
-                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), sm.getText4()));
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), LatCyrUtils.toLatinUnaccented(sm.getText4())));
                 if ( "OR".equals(sm.getOper3()))
-                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), sm.getText4()));
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), LatCyrUtils.toLatinUnaccented(sm.getText4())));
                 if ( "NOT".equals(sm.getOper3()))
-                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), sm.getText4()));
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref4(), LatCyrUtils.toLatinUnaccented(sm.getText4())));
             }
 
             if (sm.getText5() != null && !"".equals(sm.getText5())) {
                 if ( "AND".equals(sm.getOper4()))
-                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(), sm.getText5()));
+                    retVal.must(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(),LatCyrUtils.toLatinUnaccented( sm.getText5())));
                 if ( "OR".equals(sm.getOper4()))
-                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(), sm.getText5()));
+                    retVal.should(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(), LatCyrUtils.toLatinUnaccented(sm.getText5())));
                 if ( "NOT".equals(sm.getOper4()))
-                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(), sm.getText5()));
+                    retVal.mustNot(QueryBuilders.matchPhrasePrefixQuery("prefixes." + sm.getPref5(), LatCyrUtils.toLatinUnaccented(sm.getText5())));
             }
 
             if (sm.getDepartments() != null && sm.getDepartments().size() > 0){
