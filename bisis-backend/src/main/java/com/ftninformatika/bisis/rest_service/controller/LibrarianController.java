bedb3cc58cfdb60a70f91616975c31b28b3f6335
badf00d21 <jiricekova31@gmail.com>
2018-03-26 16:37:53 +0200

    update librarian bug fixed

--- bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/controller/LibrarianController.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/rest_service/controller/LibrarianController.java
@@ -1,10 +1,11 @@
 package com.ftninformatika.bisis.rest_service.controller;
 
+import com.ftninformatika.bisis.auth.model.Authority;
 import com.ftninformatika.bisis.librarian.dto.LibrarianDTO;
 import com.ftninformatika.bisis.rest_service.repository.mongo.LibrarianRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,9 +16,7 @@ import java.util.List;
 @RequestMapping("/librarians")
 public class LibrarianController {
 
-    @Autowired
-    private LibrarianRepository librarianRepository;
-
+    @Autowired private LibrarianRepository librarianRepository;
 
     @RequestMapping(value = "/getByUsername")
     public LibrarianDTO getByUsername(@RequestParam (value = "username") String username){
@@ -40,6 +39,8 @@ public class LibrarianController {
 
         LibrarianDTO librarian = librarianRepository.getByUsername(lib.getUsername());
 
+        //rola
+        librarian.setAuthorities(Arrays.asList(new Authority[]{Authority.ROLE_ADMIN}));
         librarian.setUsername(lib.getUsername());
         librarian.setPassword(lib.getPassword());
         librarian.setIme(lib.getIme());
