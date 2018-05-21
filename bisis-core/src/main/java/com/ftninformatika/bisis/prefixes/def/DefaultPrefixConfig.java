5df95c396eca7611689bc89652d4094ad29161ce
badf00d21 <jiricekova31@gmail.com>
2018-05-21 11:37:27 +0200

    prefix SQ - signature numeration added

--- bisis-core/src/main/java/com/ftninformatika/bisis/prefixes/def/DefaultPrefixConfig.java
+++ bisis-core/src/main/java/com/ftninformatika/bisis/prefixes/def/DefaultPrefixConfig.java
@@ -149,6 +149,7 @@ public class DefaultPrefixConfig implements PrefixConfig {
     allPrefixes.add("SU");
     allPrefixes.add("SF");
     allPrefixes.add("SL");
+    allPrefixes.add("SQ");
     allPrefixes.add("SW");
     allPrefixes.add("SX");
     allPrefixes.add("SY");
