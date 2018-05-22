bb380c725fd43d921dbeb5e24ceb54efb4df9090
badf00d21 <jiricekova31@gmail.com>
2018-05-22 15:18:50 +0200

    [bisis-swing-client] test konfiguracija sada gadja novu VM u ARMUNS-u

--- bisis-swing-client/src/main/java/com/ftninformatika/bisis/config/TestConfig.java
+++ bisis-swing-client/src/main/java/com/ftninformatika/bisis/config/TestConfig.java
@@ -5,10 +5,10 @@ import com.ftninformatika.bisis.librarian.Librarian;
 public class TestConfig extends AppConfig {
 
   public TestConfig(Librarian librarian, String library, String token) {
-    super("http://94.130.24.103/bisisWS/", librarian, library, token);
+    super("http://147.91.168.59/bisisWS/", librarian, library, token);
   }
 
   public TestConfig() {
-    this.setServerUrl("http://94.130.24.103/bisisWS/");
+    this.setServerUrl("http://147.91.168.59/bisisWS/");
   }
 }
