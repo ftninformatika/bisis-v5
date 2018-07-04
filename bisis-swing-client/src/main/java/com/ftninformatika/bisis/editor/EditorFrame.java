20968dd13ab727229b4bf195ec9fa867e8ee1d61
badf00d21 <jiricekova31@gmail.com>
2018-07-04 12:06:25 +0200

    [Change #88] Default "yes" for saving records dialog

--- bisis-swing-client/src/main/java/com/ftninformatika/bisis/editor/EditorFrame.java
+++ bisis-swing-client/src/main/java/com/ftninformatika/bisis/editor/EditorFrame.java
@@ -369,7 +369,7 @@ public class EditorFrame extends JInternalFrame {
             Object[] options = {Messages.getString("EDITOR_YES"), Messages.getString("EDITOR_NO"), Messages.getString("EDITOR_CANCEL")}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
             int ret = JOptionPane.showOptionDialog(null, message, Messages.getString("EDITOR_EXITEDITOR"),  //$NON-NLS-1$
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
-                    null, options, options[2]);
+                    null, options, options[0]);
             switch (ret) {
                 case 0: // da
                     boolean saved = handleSaveRecord();
