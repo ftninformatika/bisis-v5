4214eea28a3eeed4d59b6818bd267e21fb740b39
dboberic <dboberic@uns.ac.rs>
2018-07-07 12:57:19 +0200

    omogućena pretraga po DC prefiksu. Reindexirati bazu

--- bisis-backend/src/main/java/com/ftninformatika/util/elastic/ElasticUtility.java
+++ bisis-backend/src/main/java/com/ftninformatika/util/elastic/ElasticUtility.java
@@ -5,26 +5,19 @@ import com.ftninformatika.bisis.prefixes.PrefixConverter;
 import com.ftninformatika.bisis.search.SearchModel;
 import com.ftninformatika.bisis.search.UniversalSearchModel;
 import com.ftninformatika.utils.string.LatCyrUtils;
-import org.apache.lucene.search.Sort;
-import org.apache.lucene.search.SortField;
 import org.elasticsearch.index.query.*;
-import org.elasticsearch.index.search.MatchQuery;
-import org.elasticsearch.search.sort.SortBuilders;
-import org.elasticsearch.search.sort.SortOrder;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
-import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 
 /**
  * Created by Petar on 7/17/2017.
  */
 public class ElasticUtility {
+
+    private static List<String> nottokenized = Arrays.asList("DC");
 
     public static List<String> getIdsFromElasticIterable(Iterable<ElasticPrefixEntity> elasticResponse) {
         return StreamSupport.stream(elasticResponse.spliterator(), false)
@@ -67,7 +60,7 @@ public class ElasticUtility {
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.BN", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText()))); //isbn??
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.SP", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText()))); //issn
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.SB", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText()))); //predmetne odrednice
-            retVal.should(QueryBuilders.matchPhraseQuery("prefixes.DC", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText()))); //udk??
+            retVal.should(QueryBuilders.matchPhraseQuery("prefixes.DC", LatCyrUtils.toLatinUnaccented(universalSearchModel.getSearchText()))); //udk??
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.KW", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText())));
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.PP", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText())));
             retVal.should(QueryBuilders.matchPhraseQuery("prefixes.PU", LatCyrUtils.toLatinUnaccentedWithoutStopSigns(universalSearchModel.getSearchText())));
@@ -89,28 +82,44 @@ public class ElasticUtility {
     private static QueryBuilder buildQbForField(String text, String prefix) {
         QueryBuilder qb = null;
         if (text != null && !"".equals(text)) {
+            //za netokenizirane prefikse posebno se proverava da li se pravi match ili wildcard
+            if(nottokenized.contains(prefix)){
+                if (!text.contains("*")&& !text.contains("?")) {
+                    qb = QueryBuilders.matchQuery("prefixes." + prefix, LatCyrUtils.toLatinUnaccented(text)+PrefixConverter.endPhraseFlag);
+                }else {
+                    String temp = text.replaceAll("\\*","").replaceAll("\\?","");
 
-            //ako trazi pocetak teksta
-            if (text.startsWith("~") && text.length() > 1) {
-                qb = QueryBuilders.matchPhrasePrefixQuery("prefixes." + prefix, text.substring(1)).analyzer("standard").maxExpansions(0);
-                ((QueryStringQueryBuilder) qb).autoGeneratePhraseQueries(true);
-                ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
-            }
-            //ako trazi kraj teksta, obelezeno sa 0end0
-            else if (text.endsWith("~") && text.length() > 1) {
+                    if(temp.length()>0){ //ne dozvoljavamo upit koji sadrzi samo * i ?
+                        qb = QueryBuilders.wildcardQuery("prefixes." + prefix,LatCyrUtils.toLatinUnaccented(text)+PrefixConverter.endPhraseFlag);
+                    }
+                }
+
+            }else {
+                //ako trazi pocetak teksta
+                if (text.startsWith("~") && text.length() > 1) {
+                    qb = QueryBuilders.matchPhrasePrefixQuery("prefixes." + prefix, text.substring(1)).analyzer("standard").maxExpansions(0);
+                    ((QueryStringQueryBuilder) qb).autoGeneratePhraseQueries(true);
+                    ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
+                }
+                //ako trazi kraj teksta, obelezeno sa 0end0
+                else if (text.endsWith("~") && text.length() > 1) {
 //                qb = QueryBuilders.matchQuery("prefixes." + prefix, text.substring(0, text.length() - 1)
 //                        + PrefixConverter.endPhraseFlag).analyzer("standard").maxExpansions(0);
-                qb = QueryBuilders.queryStringQuery("*" + LatCyrUtils.toLatinUnaccentedWithoutStopSigns(text) + PrefixConverter.endPhraseFlag);
+                    qb = QueryBuilders.queryStringQuery("*" + LatCyrUtils.toLatinUnaccentedWithoutStopSigns(text) + PrefixConverter.endPhraseFlag);
+                    ((QueryStringQueryBuilder) qb).defaultField("prefixes." + prefix);
+                    ((QueryStringQueryBuilder) qb).autoGeneratePhraseQueries(true);
+                    ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
+                } else {
+                    qb = QueryBuilders.queryStringQuery(LatCyrUtils.toLatinUnaccentedWithoutStopSigns(text));
                 ((QueryStringQueryBuilder) qb).defaultField("prefixes." + prefix);
                 ((QueryStringQueryBuilder) qb).autoGeneratePhraseQueries(true);
-                ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
-            } else {
-                qb = QueryBuilders.queryStringQuery(LatCyrUtils.toLatinUnaccentedWithoutStopSigns(text));
-                ((QueryStringQueryBuilder) qb).defaultField("prefixes." + prefix);
-                ((QueryStringQueryBuilder) qb).autoGeneratePhraseQueries(true);
-                ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
+               ((QueryStringQueryBuilder) qb).defaultOperator(QueryStringQueryBuilder.Operator.AND);
+
+
+                }
             }
         }
+
         return qb;
     }
 
@@ -129,7 +138,7 @@ public class ElasticUtility {
             if (sm.getText1() != null && !"".equals(sm.getText1())) {
 
                 QueryBuilder qb = buildQbForField(sm.getText1(), sm.getPref1());
-
+                if(qb!=null){
                 if ("AND".equals(sm.getOper1()))
                     retVal.must(qb);
                 if ("OR".equals(sm.getOper1())) {
@@ -137,57 +146,60 @@ public class ElasticUtility {
                 }
                 if ("NOT".equals(sm.getOper1()))
                     retVal.mustNot(qb);
+            }
             }
 
             if (sm.getText2() != null && !"".equals(sm.getText2())) {
 
                 QueryBuilder qb = buildQbForField(sm.getText2(), sm.getPref2());
-
-                if ("AND".equals(sm.getOper1()))
-                    retVal.must(qb);
-                if ("OR".equals(sm.getOper1())) {
-                    retVal.should(qb);
+                if (qb != null) {
+                    if ("AND".equals(sm.getOper1()))
+                        retVal.must(qb);
+                    if ("OR".equals(sm.getOper1())) {
+                        retVal.should(qb);
+                    }
+                    if ("NOT".equals(sm.getOper1()))
+                        retVal.mustNot(qb);
                 }
-                if ("NOT".equals(sm.getOper1()))
-                    retVal.mustNot(qb);
             }
 
             if (sm.getText3() != null && !"".equals(sm.getText3())) {
 
                 QueryBuilder qb = buildQbForField(sm.getText3(), sm.getPref3());
-
-                if ("AND".equals(sm.getOper2()))
-                    retVal.must(qb);
-                if ("OR".equals(sm.getOper2()))
-                    retVal.should(qb);
-                if ("NOT".equals(sm.getOper2()))
-                    retVal.mustNot(qb);
+                if (qb != null) {
+                    if ("AND".equals(sm.getOper2()))
+                        retVal.must(qb);
+                    if ("OR".equals(sm.getOper2()))
+                        retVal.should(qb);
+                    if ("NOT".equals(sm.getOper2()))
+                        retVal.mustNot(qb);
+                }
             }
-
             if (sm.getText4() != null && !"".equals(sm.getText4())) {
 
                 QueryBuilder qb = buildQbForField(sm.getText4(), sm.getPref4());
-
-                if ("AND".equals(sm.getOper3()))
-                    retVal.must(qb);
-                if ("OR".equals(sm.getOper3()))
-                    retVal.should(qb);
-                if ("NOT".equals(sm.getOper3()))
-                    retVal.mustNot(qb);
+                if (qb != null) {
+                    if ("AND".equals(sm.getOper3()))
+                        retVal.must(qb);
+                    if ("OR".equals(sm.getOper3()))
+                        retVal.should(qb);
+                    if ("NOT".equals(sm.getOper3()))
+                        retVal.mustNot(qb);
+                }
             }
 
             if (sm.getText5() != null && !"".equals(sm.getText5())) {
 
                 QueryBuilder qb = buildQbForField(sm.getText5(), sm.getPref5());
-
-                if ("AND".equals(sm.getOper4()))
-                    retVal.must(qb);
-                if ("OR".equals(sm.getOper4()))
-                    retVal.should(qb);
-                if ("NOT".equals(sm.getOper4()))
-                    retVal.mustNot(qb);
+                if (qb != null) {
+                    if ("AND".equals(sm.getOper4()))
+                        retVal.must(qb);
+                    if ("OR".equals(sm.getOper4()))
+                        retVal.should(qb);
+                    if ("NOT".equals(sm.getOper4()))
+                        retVal.mustNot(qb);
+                }
             }
-
             retVal.minimumNumberShouldMatch(1);
 
             if (sm.getDepartments() != null && sm.getDepartments().size() > 0) {
@@ -200,7 +212,7 @@ public class ElasticUtility {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
-
+        System.out.println(retVal.toString());
         return retVal;
     }
 
