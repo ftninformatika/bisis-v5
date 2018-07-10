5089310c583792743056f176499ef89c69348946
badf00d21 <jiricekova31@gmail.com>
2018-07-10 12:18:47 +0200

    [Task #94] OPAC - unimarc record preview, secondary fields

--- bisis-angular-client/src/app/components/bisis-search/record-view/record-view.component.ts
+++ bisis-angular-client/src/app/components/bisis-search/record-view/record-view.component.ts
@@ -6,6 +6,7 @@ import {BisisSearchService} from "../../../service/bisis-search.service";
 import {CodersService} from "../../../service/coders.service";
 import {PresentItemGenerator} from "../../../tools/PresentItemGenerator";
 import {PresentItem} from "../../../model/PresentItem";
+import {setUpControl} from "@angular/forms/src/directives/shared";
 @Component({
   selector: 'app-record-view',
   templateUrl: './record-view.component.html',
@@ -17,6 +18,7 @@ export class RecordViewComponent  {
   viewTypes: SelectItem[];
   selectedViewType: string;
   unimarcRows: any[];
+  unimarcRows2: any[];
   isPage: boolean;
   presentItems: PresentItem[];
 
@@ -69,17 +71,18 @@ export class RecordViewComponent  {
             response => {
                 //generisi prikazne elemente
                 this.presentItems = this.presentItemGenerator.generatePresentItemsList(this.selectedRec['fullRecord'], response, this.selectedRec['listOfItems'] );
-                console.log(this.presentItems);
             }
         );
     }
   }
+
 
   makeUnimarc(record: any) {
     if(!record) {
       //console.log('something went wrong');
       return;
     }
+
 
     var retVal = new Array();
 
@@ -92,16 +95,55 @@ export class RecordViewComponent  {
       } else {
         el['ind1'] = element.ind1;
       }
-      if (element.ind2 == undefined || element.ind2 == null || element.ind2 == ' ' || element.ind2 == '' ) {
-        el['ind2'] = '#';
-      } else {
-        el['ind2'] = element.ind2;
+        if (element.ind2 == undefined || element.ind2 == null || element.ind2 == ' ' || element.ind2 == '' ) {
+            el['ind2'] = '#';
+        } else {
+            el['ind2'] = element.ind2;
       }
-      el['subfields'] = []
+
+      el['subfields'] = [];
       element.subfields.forEach(e => {
-        el['subfields'].push( {"name": ' [' + e.name + ']', "content": e.content});
-        //el['subfields']['content'] = e.content;
+
+        let subsubfields = [];
+        e.subsubfields.forEach(
+            ssf => {
+              subsubfields.push({"name": ' [' + ssf.name + ']', "content": ssf.content});
+            }
+        );
+
+        let secField = null;
+        if (e.secField != null && e.secField != undefined && e.secField != '' && e.secField != ' ') {
+          secField = {};
+          secField['name'] = e.secField['name']
+          if (e.secField.ind1 == undefined || e.secField.ind1 == null || e.secField.ind1 == ' ' || e.secField.ind1 == '' ) {
+              secField['ind1'] = '#';
+          } else {
+              secField['ind1'] = element.ind1;
+          }
+          if (e.secField.ind2 == undefined || e.secField.ind2 == null || e.secField.ind2 == ' ' || e.secField.ind2 == '' ) {
+              secField['ind2'] = '#';
+          } else {
+              secField['ind2'] = element.ind2;
+          }
+          let secsfs = []
+          e.secField.subfields.forEach(
+              scsf => {
+                secsfs.push({"name": '[' + scsf.name +']', "content": scsf.content})
+              }
+
+          );
+          secField['subfields'] = secsfs;
+        }
+
+        el['subfields'].push( {
+            "name": ' [' + e.name + ']',
+            "content": e.content,
+            "subsubfields": subsubfields,
+            "secField": secField
+        });
+
       });
+       console.log(el);
        retVal.push(el);
 
     });
