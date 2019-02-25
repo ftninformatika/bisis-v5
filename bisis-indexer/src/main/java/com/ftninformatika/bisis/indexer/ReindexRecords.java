b99f586539df6af63a93736f3cffa6325411243d
badf00d21 <jiricekova31@gmail.com>
2019-02-25 08:38:24 -0500

    [bisis-indexer] update

--- bisis-indexer/src/main/java/com/ftninformatika/bisis/indexer/ReindexRecords.java
+++ bisis-indexer/src/main/java/com/ftninformatika/bisis/indexer/ReindexRecords.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -26,19 +27,22 @@ import java.util.List;
 import java.util.Map;
 
 @ComponentScan("com.ftninformatika")
-@EnableElasticsearchRepositories(basePackages = "com.ftninformatika")
-@EnableMongoRepositories(basePackages = "com.ftninformatika")
 public class ReindexRecords {
 
     private static Logger log = Logger.getLogger(ReindexRecords.class);
-    public static final String INDEX_SUFIX ="_library_domain";
+    public static final String INDEX_SUFIX ="library_domain";
 
     public static void main(String[] args) {
 
-        PropertyConfigurator.configure(
-        ReindexRecords.class.getResourceAsStream("/log4j.properties"));
-        Logger.getLogger(ReindexRecords.class).info("BISIS5 record indexer starting...");
+        if (args.length < 1) {
+            System.out.println("enter library prefix, for example all (for all libraries), bgb, gbns, bs,..");
+            return;
+        }
+        String library_prefix = args[0];
 
+        PropertyConfigurator.configure(
+                ReindexRecords.class.getResourceAsStream("/log4j.properties"));
+        Logger.getLogger(ReindexRecords.class).info("BISIS5 record indexer starting...");
 
         ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
         root.setLevel(ch.qos.logback.classic.Level.INFO);
@@ -51,50 +55,59 @@ public class ReindexRecords {
         ctx.refresh();
 
         List<LibraryConfiguration> libconfigs = ctx.getBean(LibraryConfigurationRepository.class).findAll();
-        LibraryPrefixProvider libProvider = ctx.getBean(LibraryPrefixProvider.class);
 
-        log.info("Starting indexing all libraries!");
-        for (LibraryConfiguration lc : libconfigs) {
-            libProvider.setPrefix(lc.getLibraryName());
-            RecordsRepository recordsRepository = ctx.getBean(RecordsRepository.class);
-            ElasticRecordsRepository elasticRecordsRepository = ctx.getBean(ElasticRecordsRepository.class);
-            ElasticsearchTemplate elasticsearchTemplate = ctx.getBean(ElasticsearchTemplate.class);
+        log.info("Starting indexing!");
+        System.out.println("Starting indexing!");
 
-            try {
-                elasticsearchTemplate.deleteIndex(lc.getLibraryName()+INDEX_SUFIX);
-                log.info("Deleted index for library: " + lc.getLibraryName());
-                elasticsearchTemplate.createIndex(ElasticPrefixEntity.class);
-                log.info("Created index for library: " + lc.getLibraryName());
-                elasticsearchTemplate.putMapping(ElasticPrefixEntity.class);
+        if (library_prefix.equalsIgnoreCase("all")) {
+            for (LibraryConfiguration lc : libconfigs) {
+                indexLibrary(lc.getLibraryName(), ctx);
             }
-            catch (IndexNotFoundException e){
-                elasticsearchTemplate.createIndex(ElasticPrefixEntity.class);
-                elasticsearchTemplate.putMapping(ElasticPrefixEntity.class);
-                log.info("Created index for library: " + lc.getLibraryName());
-            }
-
-
-            long num = recordsRepository.count();
-            int count = 0;
-            Pageable p = new PageRequest(0, 1000);
-            Page<Record> lr = recordsRepository.findAll(p);
-
-            while (lr.hasNext()) {
-                List<ElasticPrefixEntity> ep = new ArrayList<>();
-                for (Record rec : lr) {
-                    Map<String, List<String>> prefixes = PrefixConverter.toMap(rec, null);
-                    ElasticPrefixEntity ee = new ElasticPrefixEntity(rec.get_id(), prefixes);
-                    ep.add(ee);
+        } else {
+            boolean found = false;
+            for (LibraryConfiguration lc: libconfigs) {
+                if (lc.getLibraryName().equalsIgnoreCase(library_prefix)) {
+                    indexLibrary(library_prefix.toLowerCase(), ctx);
+                    found = true;
                 }
-                
-                elasticRecordsRepository.saveAll(ep);
-                count += 1000;
-                System.out.println("Processed " + count + " of " + num + " records! Library: " + lc.getLibraryName());
-                log.info("Processed " + count + " of " + num + " records! Library: " + lc.getLibraryName());
-                lr = recordsRepository.findAll(lr.nextPageable());
+            }
+            if (!found) {
+                System.out.println("Library with that prefix does not exist!");
+                return;
             }
 
-            // resto
+        }
+
+        System.out.println("Finished indexing!!!");
+        log.info("Finished indexing!");
+    }
+
+
+    private static void indexLibrary(String libraryName, AnnotationConfigApplicationContext ctx) {
+        LibraryPrefixProvider libProvider = ctx.getBean(LibraryPrefixProvider.class);
+        libProvider.setPrefix(libraryName);
+        RecordsRepository recordsRepository = ctx.getBean(RecordsRepository.class);
+        ElasticRecordsRepository elasticRecordsRepository = ctx.getBean(ElasticRecordsRepository.class);
+        ElasticsearchTemplate elasticsearchTemplate = ctx.getBean(ElasticsearchTemplate.class);
+
+        try {
+            elasticsearchTemplate.deleteIndex(libraryName + INDEX_SUFIX);
+            log.info("Deleted index for library: " + libraryName);
+            elasticsearchTemplate.createIndex(ElasticPrefixEntity.class);
+            log.info("Created index for library: " + libraryName);
+            elasticsearchTemplate.putMapping(ElasticPrefixEntity.class);
+        } catch (IndexNotFoundException e) {
+            elasticsearchTemplate.createIndex(ElasticPrefixEntity.class);
+            elasticsearchTemplate.putMapping(ElasticPrefixEntity.class);
+            log.info("Created index for library: " + libraryName);
+        }
+
+        long num = recordsRepository.count();
+        int count = 0;
+        Pageable p = PageRequest.of(0, 1000);
+        Page<Record> lr = recordsRepository.findAll(p);
+        int pages = lr.getTotalPages();
+        for (int i = 0; i < pages; i++) {
             List<ElasticPrefixEntity> ep = new ArrayList<>();
             for (Record rec : lr) {
                 Map<String, List<String>> prefixes = PrefixConverter.toMap(rec, null);
@@ -102,11 +115,16 @@ public class ReindexRecords {
                 ep.add(ee);
             }
             elasticRecordsRepository.saveAll(ep);
-            System.out.println("Processed " + num + " of " + num + " records! Library: " + lc.getLibraryName());
-            log.info("Processed " + count + " of " + num + " records! Library: " + lc.getLibraryName());
+
+            count += ep.size();
+            System.out.println("Processed " + count + " of " + num + " records! Library: " + libraryName);
+            log.info("Processed " + count + " of " + num + " records! Library: " + libraryName);
+            if (!lr.isLast()) {
+                p = lr.nextPageable();
+                lr = recordsRepository.findAll(p);
+            }
         }
-        System.out.println("Finished indexing!!!");
-        log.info("Finished indexing!");
+        elasticsearchTemplate.getClient().admin().indices().prepareForceMerge(libraryName + INDEX_SUFIX).setMaxNumSegments(1).execute().actionGet();
     }
 
 }
