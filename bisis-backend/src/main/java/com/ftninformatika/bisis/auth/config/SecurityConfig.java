e02f54a5b75835de5e6368846c8ac0cb439bfa41
badf00d21 <jiricekova31@gmail.com>
2019-12-06 11:25:57 +0100

    Security - Role based endpoint access

--- bisis-backend/src/main/java/com/ftninformatika/bisis/auth/config/SecurityConfig.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/auth/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.ftninformatika.bisis.auth.config;
 
-import com.ftninformatika.bisis.auth.model.Authority;
 import com.ftninformatika.bisis.auth.security.filter.AuthenticationTokenFilter;
 import com.ftninformatika.bisis.auth.security.service.TokenAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,41 +30,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //TODO: update security config before deploy!
         http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers(
                         "/book",
                         "/book/multiple",
                         "/book/collection",
-                        "/hello",
                         "/opac/**",
                         "/external_hit/**",
                         "/auth",
-                        "/book_cover/**",
+                        "/book_cover/retrieve/**",
                         "/book_common/**",
                         "/memauth",
-                        "/members/lending_history/**",
-                        "/members/active_lendings/**",
                         "/coders/language",
                         "/coders/lib_configurations",
                         "/records/wrapperrec/**",
                         "/records/wrapperrec/universal",
                         "/records/unimarc",
                         "/records/query/**",
-                        "/book_cover/**",
                         "/v2/api-docs",
                         "/configuration/**",
                         "/swagger*/**",
                         "/webjars/**",
                         "/records/opac_wrapperrec/**",
-                        "/library_members/**",
+                        "/library_members/get_member_by_activation_token/**",
+                        "/library_members/activate_account/**",
+                        "/library_members/forgot_password/**",
+                        "/library_members/activate_account/**",
                         "/coders/location**",
-                        "/records/rate_record/**",
                         "/coders/item_status**",
                         "/coders/sublocation/get_by_location**").permitAll()
                 .antMatchers(
                         "/members_repository/**",
-                        "/circ_report/**")
-                .hasAnyRole("USER","ADMIN")
-                .antMatchers("/**").hasAuthority("ROLE_ADMIN")
+                        "/circ_report/**",
+                        "/library_members/**",
+                        "/members/active_lendings/**",
+                        "/members/lending_history/**",
+                        "/records/rate_record/**"
+                        )
+                .hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+                .antMatchers(
+
+                        "/book_cover/upload/**",
+                        "/**").hasAuthority("ROLE_ADMIN")
 //                .anyRequest().authenticated()
                 //.anyRequest().permitAll()
                 .and()
