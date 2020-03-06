8539e6319fb19d03c9facde81105937de5319d7c
badf00d21 <jiricekova31@gmail.com>
2020-03-06 10:51:02 +0100

    ++

--- bisis-swing-client/src/main/java/com/ftninformatika/bisis/LibraryList.java
+++ bisis-swing-client/src/main/java/com/ftninformatika/bisis/LibraryList.java
@@ -1,7 +1,7 @@
 package com.ftninformatika.bisis;
 
 public class LibraryList {
-    public static final String[] LIB_PREFIXES = {"bs", "gbns", "bgb", "msk", "bmb", "kru"};
+    public static final String[] LIB_PREFIXES = {"bs", "gbns", "bgb", "msk", "bmb", "kru", "bmg"};
 
     public static boolean isRegistryEnabled(String lib){
         if (!lib.equals("bgb"))
