228f320c712edaa9b8512b6e67d25fa7efc45329
badf00d21 <petar.makevic@ftninformatika.com>
2020-08-17 16:41:51 +0200

    [Feature #190] Fixed problem finding libpcslite for Linux distros

--- bisis-core/src/main/java/com/ftninformatika/bisis/ecard/ElCardReader.java
+++ bisis-core/src/main/java/com/ftninformatika/bisis/ecard/ElCardReader.java
@@ -1,11 +1,30 @@
 package com.ftninformatika.bisis.ecard;
 
+import com.ftninformatika.utils.OsCheck;
 import net.devbase.jfreesteel.EidCard;
 import net.devbase.jfreesteel.EidInfo;
 
 import javax.smartcardio.*;
 
 public class ElCardReader {
+
+    private static final String ARCH_MODEL_PARAM = "sun.arch.data.model";
+    private static final String LIBPCS_PARAM = "sun.security.smartcardio.library";
+    private static final String LINUX_64_LIBPCSLITE_PATH = "/lib/x86_64-linux-gnu/libpcsclite.so.1";
+    private static final String LINUX_32_LIBPCSLITE_PATH = "/lib/i386-linux-gnu/libpcsclite.so.1";
+
+    static {
+        OsCheck.OSType osType = OsCheck.getOperatingSystemType();
+        if (osType == OsCheck.OSType.Linux) {
+            String archModel = System.getProperty(ARCH_MODEL_PARAM);
+            if (archModel != null) {
+                if (archModel.equals("32"))
+                    System.setProperty(LIBPCS_PARAM, LINUX_32_LIBPCSLITE_PATH);
+                else if (archModel.equals("64"))
+                    System.setProperty(LIBPCS_PARAM, LINUX_64_LIBPCSLITE_PATH);
+            }
+        }
+    }
 
     private static ElCardReader instance;
     private CardTerminal cardTerminal;
