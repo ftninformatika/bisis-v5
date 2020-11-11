e6604dc93eda796732c13feaaea783642e0f06ea
danijela <tesendic>
2020-11-11 14:05:23 +0100

    wip

---
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.ftninformatika.bisis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("developmentSingle")
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");;
+            }
+        };
+    }
+}
