568c4d963214dba463ba38a7521e6e65466849dc
danijela <tesendic>
2021-01-05 16:18:46 +0100

    request new token when expired, remove librarians from menu

--- bisis-authentication/src/main/java/com/ftninformatika/bisisauthentication/security/JWTUtil.java
+++ bisis-authentication/src/main/java/com/ftninformatika/bisisauthentication/security/JWTUtil.java
@@ -61,7 +61,7 @@ public class JWTUtil {
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12))
                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
     }
 
