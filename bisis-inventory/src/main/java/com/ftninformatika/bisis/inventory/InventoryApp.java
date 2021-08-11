17a1ecbff81d50e97cdd5aca7faf02c781f03717
danijela <dacafaca@gmail.com>
2021-08-11 16:26:01 +0200

    gradle fix

--- bisis-inventory/src/main/java/com/ftninformatika/bisis/inventory/InventoryApp.java
+++ bisis-inventory/src/main/java/com/ftninformatika/bisis/inventory/InventoryApp.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories("com.ftninformatika")
-@EnableElasticsearchRepositories("com.ftninformatika")
 @ComponentScan("com.ftninformatika")
 @SpringBootApplication
 public class InventoryApp extends SpringBootServletInitializer {
