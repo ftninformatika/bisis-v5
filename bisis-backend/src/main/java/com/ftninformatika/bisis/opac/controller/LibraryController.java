35ae25f0476fe06d812c702210129ab587341260
Danijela Boberic <dboberic@uns.ac.rs>
2021-08-13 16:46:20 +0200

    library service fix

--- bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/LibraryController.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/LibraryController.java
@@ -2,6 +2,7 @@ package com.ftninformatika.bisis.opac.controller;
 
 import com.ftninformatika.bisis.opac.Library;
 import com.ftninformatika.bisis.opac.repository.LibraryRepository;
+import com.ftninformatika.utils.LibraryPrefixProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +13,11 @@ import java.util.List;
 public class LibraryController {
     @Autowired
     LibraryRepository libraryRepository;
-    @GetMapping("/get/{libPrefix}")
-    public List<Library> getLibraries(@PathVariable("libPrefix") String libPrefix){
-        return libraryRepository.findLibraryByPrefix(libPrefix);
+    @Autowired
+    LibraryPrefixProvider libraryPrefixProvider;
+    @GetMapping("/get")
+    public List<Library> getLibraries(){
+        return libraryRepository.findLibraryByPrefix(libraryPrefixProvider.getLibPrefix());
     }
     @PostMapping("/add")
     public Library addLibrary(@RequestBody Library library){
