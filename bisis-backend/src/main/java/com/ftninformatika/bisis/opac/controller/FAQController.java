cbc7086ebd61c8b5093145aa55d1a29b2a7b2638
marija.kovacevic <marijakovacevic995@gmail.com>
2021-09-02 00:50:25 +0200

    [bisis-backend] Dodavanje novog faq

--- bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/FAQController.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/FAQController.java
@@ -2,9 +2,9 @@ package com.ftninformatika.bisis.opac.controller;
 
 import com.ftninformatika.bisis.opac.repository.FAQRepository;
 import com.ftninformatika.bisis.opac.FAQ;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -17,7 +17,17 @@ public class FAQController {
     FAQRepository faqRepository;
 
     @GetMapping("/get")
-    public List<FAQ> getFAQ(){
+    public List<FAQ> getFAQ() {
         return faqRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<FAQ> addFAQ(@RequestBody FAQ faq) {
+        try {
+            FAQ createdFaq = faqRepository.save(faq);
+            return new ResponseEntity<>(createdFaq, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
