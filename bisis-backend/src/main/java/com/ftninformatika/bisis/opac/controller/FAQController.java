7bc89d769b1cf8d23bda2286d26f8e586783f0b8
marija.kovacevic <marijakovacevic995@gmail.com>
2021-09-02 18:52:18 +0200

    [bisis-backend] Izmena i brisanje FAQ

--- bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/FAQController.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/opac/controller/FAQController.java
@@ -30,4 +30,31 @@ public class FAQController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping
+    public ResponseEntity<FAQ> editFAQ(@RequestBody FAQ faq) {
+        if (!faqRepository.findById(faq.get_id()).isPresent()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        try {
+            FAQ createdFaq = faqRepository.save(faq);
+            return new ResponseEntity<>(createdFaq, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("{faqId}")
+    public ResponseEntity<Boolean> deleteFAQ(@PathVariable String faqId) {
+        if (!faqRepository.findById(faqId).isPresent()) {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+        try {
+            faqRepository.deleteById(faqId);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
