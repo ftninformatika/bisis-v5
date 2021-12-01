c86e991f4925a270143c4c268234f174c1eaa3a5
marija.kovacevic <marijakovacevic995@gmail.com>
2021-12-01 14:59:00 +0100

    Promenjen response status za availability endpoint za mobile

--- bisis-backend/src/main/java/com/ftninformatika/bisis/exception/model/ReservationNotSupportedException.java
+++ bisis-backend/src/main/java/com/ftninformatika/bisis/exception/model/ReservationNotSupportedException.java
@@ -4,7 +4,7 @@ package com.ftninformatika.bisis.exception.model;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
 public class ReservationNotSupportedException extends RuntimeException {
     public ReservationNotSupportedException() {
         super("Библиотека не подржава ову функционалност");
