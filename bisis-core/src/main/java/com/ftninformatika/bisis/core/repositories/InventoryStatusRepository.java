dd3c57c0856d2bfee8eee90ac086063456b55c88
Danijela Boberic <dboberic@uns.ac.rs>
2023-05-26 18:44:07 +0200

    promena kodera u reviziji

--- bisis-core/src/main/java/com/ftninformatika/bisis/core/repositories/InventoryStatusRepository.java
+++ bisis-core/src/main/java/com/ftninformatika/bisis/core/repositories/InventoryStatusRepository.java
@@ -1,15 +1,9 @@
 package com.ftninformatika.bisis.core.repositories;
 
 import com.ftninformatika.bisis.inventory.InventoryStatus;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.List;
-
-public interface InventoryStatusRepository extends MongoRepository<InventoryStatus, String> {
-
-    @Query("{'$or': [{'library':{'$exists': false}},{'library':?0}]}")
-    public List<InventoryStatus> getCoders(String libName);
+public interface InventoryStatusRepository extends CoderRepository<InventoryStatus> {
 
     @Query("{'coder_id': ?0}")
     InventoryStatus getByCoder_Id(String coder_id);
